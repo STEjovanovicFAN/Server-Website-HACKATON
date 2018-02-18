45e4d8639776cb0c4c9741e171254d5328bdc78e
Bigfootmandude <davidjkeizer@gmail.com>
2018-02-18 12:43:35 -0700

    Update YouHaveMail.ino

--- YouHaveMail.ino
+++ YouHaveMail.ino
@@ -5,20 +5,25 @@ STARTUP(cellular_credentials_set("isp.telus.com", "", "", NULL));
 
 //First we will need to declare which pins are used for reading and writing
 //As well as set some constants.
-
 #define TIMEOUT 1000
 #define CTRL_Z 0x1A
-char szPhoneNumber[14]= "+********";
 
+
+\\Set up the pins
 int motionPower = D0;
 int motionInput = A1;
 int boardLed = D7;
+int timeOut = 20000; //unused
 
-int led1 = D6;
 
+
+int power = D0;
+int resetButtonOut = C1;
+int resetButtonInput = C3;
+int led1 = D6; //unused
 long delayLength = 1000;
 
-
+//unused
 bool motionDetected = false;
 
 
@@ -29,33 +34,21 @@ int callback(int type, const char* buf, int len, char* param){
 	return WAIT;
 }
 
-int sendMessage(char* pMessage){
-    char szCmd[64];
-    
-    sprintf(szCmd, "AT+CMGS=\"+%s\",145\r\n", szPhoneNumber);
-    
-    Serial.print("Sending command ");
-    Serial.print(szCmd);
-    Serial.println();
-    
-    char szReturn[32] = "";
-    
-    Cellular.command(callback, szReturn, TIMEOUT, "AT+CMGF=1\r\n");
-    Cellular.command(callback, szReturn, TIMEOUT, szCmd);
-    Cellular.command(callback, szReturn, TIMEOUT, pMessage);
-    
-    sprintf(szCmd, "%c", CTRL_Z);
-    
-    int retVal = Cellular.command(callback, szReturn, TIMEOUT, szCmd);
-    
-    if(RESP_OK == retVal){
-        Serial.println("+OK, Message Send");
-    }
-    else{
-        Serial.println("+ERROR, error sending message");
-    }
-    
-    return retVal;
+
+//unused
+void text2(){
+
+char message[] = "testing";
+char* pMessage= message;
+
+char szReturn[32] = "";
+//Cellular.command(callback, szReturn, TIMEOUT,"AT+CMGS=\"+14038757188\"");
+//Cellular.command(callback, szReturn, TIMEOUT, "Reset! 0x1A");
+
+char szCmd[64];
+
+
+
 }
 
 void setup() {
@@ -67,9 +60,14 @@ pinMode(motionInput, INPUT);
 pinMode(boardLed, OUTPUT);
 pinMode(led1, OUTPUT);
 
+pinMode(resetButtonInput, INPUT);
+pinMode(resetButtonOut, OUTPUT);
+
 
 digitalWrite(motionPower, HIGH);
+digitalWrite(resetButtonInput, HIGH);
 //Wait for 1 minute for it to set up (warm up?);
+
 
 delay(60000);
 
@@ -81,19 +79,22 @@ digitalWrite(boardLed, HIGH);
 }
 
 
+
+
+//send a text to my phone number
 void text(){
 
 char message[] = "testing";
 char* pMessage= message;
 
 char szReturn[32] = "";
-//Cellular.command(callback, szReturn, TIMEOUT,"AT+CMGS=\"+1******\"");
+//Cellular.command(callback, szReturn, TIMEOUT,"AT+CMGS=\"+14038757188\"");
 //Cellular.command(callback, szReturn, TIMEOUT, "Testing! 0x1A");
 
 char szCmd[64];
 
 Cellular.command(callback, szReturn, TIMEOUT, "AT+CMGF=1\r\n");
-Cellular.command(callback, szReturn, TIMEOUT, "AT+CMGS=\"+1******INSERTPHONENUMBERHERE*******\" \r\n");
+Cellular.command(callback, szReturn, TIMEOUT, "AT+CMGS=\"+14038077104\" \r\n");
 Cellular.command(callback, szReturn, TIMEOUT, "You have mail!");
   sprintf(szCmd, "%c", CTRL_Z);
 Cellular.command(callback, szReturn, TIMEOUT, szCmd);
@@ -102,17 +103,27 @@ Cellular.command(callback, szReturn, TIMEOUT, szCmd);
 
 
 
+
+
+
+bool disabled = false;
+
+//if motion send text
+
 void loop () {
 	
-	if(digitalRead(motionInput) == HIGH){
-		digitalWrite(led1, HIGH);
-		text();
-	}
-	else{
-		digitalWrite(led1, LOW);
-	}
+	if(!disabled){
+		if(digitalRead(motionInput) == HIGH){
+			
+			text();
+			digitalWrite(boardLed, LOW);
+			
+		}
+		
+			
+		
 	
-	
+	}
 	
 }
 
